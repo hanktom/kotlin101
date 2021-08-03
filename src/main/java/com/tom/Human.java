2306b5db2a590e6830e20bbba83aa5aa9f0c0c78
Hank Tom <hanktom@gmail.com>
2021-08-03 21:44:32 +0800

    all about variable, data type, class design, null

---
+++ src/main/java/com/tom/Human.java
@@ -0,0 +1,11 @@
+package com.tom;
+
+public class Human {
+    String name;
+
+    public static void main(String[] args) {
+        Human human = new Human();
+//        human.name = "Jack";
+        System.out.println(human.name.length());
+    }
+}
--- src/main/kotlin/Tester.kt
+++ src/main/kotlin/Tester.kt
@@ -1,8 +1,35 @@
 //Top level
 fun main() {
     println("Hello world")
+    //Int, Short, Long, Byte, Char
+    //Float, Double, Boolean
+    val age:Int = 18
+    var ball = 3
+    ball += 2
+    var isOnSale = false
+    println(ball)
+    isOnSale = true
+    var name:String? = "Jack"
+    name = null
+    println(age > -3 && age < 5)
+//    Tester test = new Tester();
+//    int n = 10;
+//    Tester()
+    val person = Person()
+    person.hello()
+    person.name = "Hank"
+    person.name = null
+    //Null check
+    val length = person.name?.length
+    println(length)
+//    person = Person() go wrong!
 }
 
-class Tester {
-
+class Person {
+//    String name;
+    var name:String? = null
+    //public void hello() { .. }
+    fun hello() {
+        println("Hello Kotlin from Person")
+    }
 }
