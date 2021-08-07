db363d7aa12e3c2d9830e74e8c969dbed093875d
Hank Tom <hanktom@gmail.com>
2021-08-07 15:40:13 +0800

    Guessing Better version

---
+++ src/main/kotlin/Guessing2.kt
@@ -0,0 +1,32 @@
+package com.tom.guess
+
+import java.util.*
+
+fun main() {
+    val secretNumber = SecretNumber()
+    var num = 0
+    var diff = -1
+    while(diff != 0) {
+        print("Please enter a number: ")
+        var line = readLine()
+        num = line?.toIntOrNull()?: 0
+        diff = secretNumber.guess(num)
+        if (diff > 0) {
+            println("Smaller")
+        } else if (diff < 0) {
+            println("Bigger")
+        } else {
+            println("You got it")
+        }
+    }
+    println("Times: ${secretNumber.counter}")
+}
+
+class SecretNumber {
+    var counter = 0
+    private var secret = Random().nextInt(10)+1
+    fun guess(num: Int) : Int {
+        counter++
+        return num - secret
+    }
+}
